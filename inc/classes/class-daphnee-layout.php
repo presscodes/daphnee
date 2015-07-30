a781bcb57222cdcfb0a712d3dba9af0e366ddf5f
Aristeides Stathopoulos <aristath@gmail.com>
2015-07-30 19:06:04 +0300

    added layout options.

--- inc/classes/class-daphnee-layout.php
+++ inc/classes/class-daphnee-layout.php
@@ -2,12 +2,39 @@
 
 class Daphnee_Layout {
 
+	public function __construct() {
+		$this->remove_sidebar_on_fullwidth();
+		add_action( 'wp_enqueue_scripts', array( $this, 'left_sidebar_css' ) );
+	}
+
 	public function main_content_columns() {
-		return 'col_9';
+		if ( 'full-screen' == get_theme_mod( 'layout', 'right-sidebar' ) ) {
+			return 'col_12';
+		} else {
+			return 'col_' . ( 12 - intval( get_theme_mod( 'sidebar_width', 3 ) ) );
+		}
 	}
 
 	public function sidebar_columns() {
-		return 'col_3';
+		if ( 'full-screen' == get_theme_mod( 'layout', 'right-sidebar' ) ) {
+			return null;;
+		} else {
+			return 'col_' . intval( get_theme_mod( 'sidebar_width', 3 ) );
+		}
+	}
+
+	public function remove_sidebar_on_fullwidth() {
+		if ( 'full-screen' == get_theme_mod( 'layout', 'right-sidebar' ) ) {
+			add_filter( 'daphnee/template/sidebar', '__return_false' );
+		}
+	}
+
+	public function left_sidebar_css() {
+		$css = '';
+		if ( 'left-sidebar' == get_theme_mod( 'layout', 'right-sidebar' ) ) {
+			$css = '#primary{order:2}';
+		}
+		wp_add_inline_style( 'daphnee-style', $css );
 	}
 
 }
--- inc/customizer.php
+++ inc/customizer.php
@@ -24,3 +24,98 @@ function daphnee_customize_preview_js() {
 	wp_enqueue_script( 'daphnee_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'daphnee_customize_preview_js' );
+
+/**
+ * Add some custom CSS to the customizer screen.
+ */
+function daphnee_customizer_css() { ?>
+	<style>
+	#customize-control-layout.customize-control-radio-image .image.ui-buttonset label {
+		max-width: 29%;
+	}
+	#customize-control-layout.customize-control-radio-image .image.ui-buttonset label img {
+		width: 100%;
+		height: auto;
+	}
+	</style>
+	<?php
+}
+add_action( 'customize_controls_print_styles', 'daphnee_customizer_css' );
+
+/**
+ * Check that Kirki exists before proceeding
+ */
+if ( class_exists( 'Kirki' ) ) {
+
+	/**
+	 * Add the configuration for the Daphnee Theme
+	 */
+	Kirki::add_config( 'daphnee', array(
+		'capability'  => 'edit_theme_options',
+		'option_type' => 'theme_mod',
+	) );
+
+	/**
+	 * Add the "layout" section
+	 */
+	Kirki::add_section( 'layout', array(
+		'title'       => __( 'Layout', 'daphnee' ),
+		'description' => __( 'Configure your layout options' ),
+	) );
+
+	Kirki::add_field( 'daphnee', array(
+	    'type'        => 'radio-image',
+	    'settings'    => 'layout',
+	    'label'       => __( 'Layout', 'daphnee' ),
+	    'section'     => 'layout',
+	    'default'     => '',
+	    'priority'    => 10,
+	    'choices'     => array(
+	        'full-screen'   => trailingslashit( get_template_directory_uri() ) . 'inc/kirki/assets/images/1c.png',
+			'left-sidebar'  => trailingslashit( get_template_directory_uri() ) . 'inc/kirki/assets/images/2cl.png',
+			'right-sidebar' => trailingslashit( get_template_directory_uri() ) . 'inc/kirki/assets/images/2cr.png',
+	    ),
+	) );
+
+	Kirki::add_field( 'daphnee', array(
+		'type'        => 'slider',
+		'settings'    => 'site_max_width',
+		'label'       => __( 'Max Width', 'daphnee' ),
+		'description' => __( 'Select the max-width for the main content area.', 'daphnee' ),
+		'section'     => 'layout',
+		'default'     => 1020,
+		'priority'    => 15,
+		'choices'     => array(
+			'min'  => 420,
+			'max'  => 1600,
+			'step' => 1
+		),
+		'output'      => array(
+			'element'  => '#content.row',
+			'property' => 'max-width',
+			'units'    => 'px',
+		)
+	) );
+
+	Kirki::add_field( 'daphnee', array(
+		'type'        => 'radio-buttonset',
+		'settings'    => 'sidebar_width',
+		'label'       => __( 'Sidebar Width (1-12 columns)', 'daphnee' ),
+		'section'     => 'layout',
+		'default'     => '4',
+		'priority'    => 10,
+		'choices'     => array(
+			'3' => __( 'Narrow', 'daphnee' ),
+			'4' => __( 'Default', 'daphnee' ),
+			'5' => __( 'Wide', 'daphnee' ),
+		),
+		'required'    => array(
+			array(
+				'setting'  => 'layout',
+				'operator' => '!=',
+				'value'    => 'full-screen',
+			)
+		)
+	) );
+
+}
