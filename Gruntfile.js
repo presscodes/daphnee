e2e3516222a11d377070d0e34986c25537ede226
Dimitris Kalliris <kalliris.d@gmail.com>
2015-08-05 20:21:17 +0300

    use Grunt.js for compiling main stylesheet

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: [{
+          'style.css': 'sass/style.scss'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['sass']);
+
+};
