fdde9d465ebc5f29e766e56b6d4c6a9346a00902
Aristeides Stathopoulos <aristath@gmail.com>
2015-08-22 06:33:40 +0300

    implement header image

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -5,12 +5,6 @@
  *
  * You can add an optional custom header image to header.php like so ...
  *
-	<?php if ( get_header_image() ) : ?>
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-	</a>
-	<?php endif; // End header image check. ?>
- *
  * @package Daphnee
  */
 
@@ -72,10 +66,10 @@ function daphnee_header_style() {
 		header nav#site-navigation a {
 			color: #<?php echo esc_attr( $header_text_color ); ?>;
 		}
-	<?php endif; 
+	<?php endif;
   if ( !empty( get_header_image() ) ) :
   	echo '#masthead{ background-image: url("' . get_header_image() . '"); }';
-  endif; 
+  endif;
 	?>
 	</style>
 	<?php
