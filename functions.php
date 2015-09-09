320a05ab2c27891b38cbede3ad1f2f5fbb8646f3
Dimitris Kalliris <kalliris.d@gmail.com>
2015-09-09 21:56:01 +0300

    add Socicons

--- functions.php
+++ functions.php
@@ -127,6 +127,8 @@ add_action( 'widgets_init', 'daphnee_widgets_init' );
 function daphnee_scripts() {
 	wp_enqueue_style( 'daphnee-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'socicon', get_template_directory_uri() . '/assets/css/socicon.css', false );
+
 	wp_enqueue_script( 'daphnee-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20120206', true );
 
 	wp_enqueue_script( 'daphnee-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
@@ -134,6 +136,7 @@ function daphnee_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
 }
 add_action( 'wp_enqueue_scripts', 'daphnee_scripts' );
 
